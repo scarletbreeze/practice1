c0c6bdda6989ebcc496ede5c55d9b6382aba66f0
gimseonhong-ui-MacBook-Air.local <gimseonhong@gimseonhong-ui-MacBook-Air.local>
2019-01-02 16:35:40 +0900

    seonhong

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HeaderContainer from "./component/header/HeaderContainer";
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <HeaderContainer />
       </div>
     );
   }
---
+++ hello-react/src/component/header/headerPresenter.js
@@ -0,0 +1,12 @@
+
+
+const HeaderPresenter = ({props, context}) => {
+
+    return(
+        <div>
+        <text>test</text>s
+        </div>
+    )
+}
+
+export default HeaderPresenter;
