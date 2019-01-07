8fadcb2bee7eccefb426d6af0c0c5f780953298d
gimseonhong-ui-MacBook-Air.local <gimseonhong@gimseonhong-ui-MacBook-Air.local>
2019-01-07 10:52:18 +0900

    seonhong nav

--- src/component/Seonhong/subnav/subNavPresenter.js
+++ src/component/Seonhong/subnav/subNavPresenter.js
@@ -7,7 +7,7 @@ const SubNavPresenter = ({props, context}) => {
     return(
         // <!-- .cd-auto-hide-header -->
     
-        <Fragment>
+        // <Fragment>
             <section class="cd-hero">
                 <div class="cd-hero-content">
                     {/* <!-- your content here --> */}
@@ -16,29 +16,29 @@ const SubNavPresenter = ({props, context}) => {
 
     
     
-            <nav class="cd-secondary-nav">
-                <ul>
-                    <li class="sub_navi_list first">
-                        <a class="active" href="#0">비전</a>
-                    </li>
-                    <li class="sub_navi_list second">
-                        <a href="#1">문화</a>
-                    </li>
-                    <li class="sub_navi_list third">
-                        <a href="#2">CI</a>
-                    </li>
-                    <li class="sub_navi_list forth">
-                        <a href="#3">보도자료</a>
-                    </li>
-                    <li class="sub_navi_list fifth">
-                        <a href="#4">공지사항</a>
-                    </li>
-                    {/* <!-- <li class="sub_navi_list sixth">
-                        <a href="#5">오시는 길</a>
-                    </li> --> */}
-                </ul>
-            </nav>
-        </Fragment>
+            // <nav class="cd-secondary-nav">
+            //     <ul>
+            //         <li class="sub_navi_list first">
+            //             <a class="active" href="#0">비전</a>
+            //         </li>
+            //         <li class="sub_navi_list second">
+            //             <a href="#1">문화</a>
+            //         </li>
+            //         <li class="sub_navi_list third">
+            //             <a href="#2">CI</a>
+            //         </li>
+            //         <li class="sub_navi_list forth">
+            //             <a href="#3">보도자료</a>
+            //         </li>
+            //         <li class="sub_navi_list fifth">
+            //             <a href="#4">공지사항</a>
+            //         </li>
+            //         {/* <!-- <li class="sub_navi_list sixth">
+            //             <a href="#5">오시는 길</a>
+            //         </li> --> */}
+            //     </ul>
+            // </nav>
+        // </Fragment>
     )
 }
 export default SubNavPresenter;
